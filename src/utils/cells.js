51d0a609d6a206e6336ef71df2438dae79f078fb
Victor Nguyen <vic.nguyen@gmail.com>
2017-03-15 22:02:30 +1100

    Change cell values to be bools
    They can only have one of two states anyway

--- src/utils/cells.js
+++ src/utils/cells.js
@@ -4,12 +4,15 @@ export function createCells({ cols = 10, rows = 5, randomise = false } = {}) {
   // TODO: replace this with native loops to demonstrate i know how to do those?
   // TODO: implement randomise param
   // TODO: is creating a flat array of objects with x/y props easier to deal with?
-  return times(rows, () => times(cols, () => 0))
+  return times(rows, () => times(cols, () => {
+    // TODO: add weight slider to generate more dead/alive cells?
+    return Math.random() >= 0.9
+  }))
 }
 
 export function toggleCell({ col, row, cells = [] } = {}) {
   try {
-    cells[row][col] = (cells[row][col] === 1) ? 0 : 1
+    cells[row][col] = !cells[row][col]
   } catch(e) {
     // TODO: throw a meaningful exception
   } finally {
