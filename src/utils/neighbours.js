de9197d2165f272215444b3212404459057205e9
Victor Nguyen <vic.nguyen@gmail.com>
2017-03-16 21:08:59 +1100

    Rename and comment neighbouring consts for clarity

--- src/utils/neighbours.js
+++ src/utils/neighbours.js
@@ -9,12 +9,13 @@ export function shouldCellLive({ x, y, cells }) {
 }
 
 function getNeighbours({ x, y, cells = [] }) {
-  const yCoords = [y - 1, y, y + 1]
-  const xCoords = [x - 1, x, x + 1]
+  // possible x and y coords for neighbouring cells
+  const rows = [y - 1, y, y + 1]
+  const cols = [x - 1, x, x + 1]
 
   return flatten(
-    yCoords.map((yPoint) => {
-      return xCoords.map((xPoint) => {
+    rows.map((yPoint) => {
+      return cols.map((xPoint) => {
         const isTargetCell = yPoint === x && xPoint === y
         if (isTargetCell) {
           return null
